d7c54b602fd552366615748bd25e4ecbbd2edf69
tapeds <frrllmatthew@gmail.com>
2024-09-22 01:46:48 +0700

    feat: add keybinds for multiple terminals

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("toggleterm").setup({
 			size = 15,
-			open_mapping = [[;t]],
+			open_mapping = [[<C-\>]],
 			hide_numbers = true,
 			shade_filetypes = {},
 			shade_terminals = true,
@@ -15,5 +15,21 @@ return {
 			direction = "float",
 			close_on_exit = true,
 		})
+
+		function _G.toggle_term_by_id(id)
+			local Terminal = require("toggleterm.terminal").Terminal
+			local term = require("toggleterm.terminal").get(id)
+			if term then
+				term:toggle()
+			else
+				local new_term = Terminal:new({ id = id, direction = "float", hidden = true })
+				new_term:toggle()
+			end
+		end
+
+		vim.api.nvim_set_keymap("n", ";t", "<cmd>lua toggle_term_by_id(1)<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("t", ";t", "<cmd>lua toggle_term_by_id(1)<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", ";g", "<cmd>lua toggle_term_by_id(2)<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("t", ";g", "<cmd>lua toggle_term_by_id(2)<CR>", { noremap = true, silent = true })
 	end,
 }
