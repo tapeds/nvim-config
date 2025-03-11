6df14ea1dcde3da458d243e2647231badf29a017
tapeds <frrllmatthew@gmail.com>
2025-03-11 09:44:44 +0700

    feat: moveline mapping to control

--- lua/plugins/moveline.lua
+++ lua/plugins/moveline.lua
@@ -2,6 +2,18 @@ return {
 	"echasnovski/mini.move",
 	version = false,
 	config = function()
-		require("mini.move").setup()
+		require("mini.move").setup({
+			mappings = {
+				left = "<C-h>",
+				right = "<C-l>",
+				down = "<C-j>",
+				up = "<C-k>",
+
+				line_left = "<C-h>",
+				line_right = "<C-l>",
+				line_down = "<C-j>",
+				line_up = "<C-k>",
+			},
+		})
 	end,
 }
