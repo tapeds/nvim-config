caeb77669536686aa17a45af9fe4064724bf66c4
tapeds <frrllmatthew@gmail.com>
2026-05-11 16:00:19 +0700

    feat: update telescope configuration to include fff.nvim plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,49 @@
+-- return {
+-- 	{
+-- 		"nvim-telescope/telescope.nvim",
+-- 		dependencies = { "nvim-lua/plenary.nvim" },
+-- 		config = function()
+-- 			local builtin = require("telescope.builtin")
+-- 			vim.keymap.set("n", ";f", builtin.find_files, {})
+-- 			vim.keymap.set("n", ";r", builtin.live_grep, {})
+-- 			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { noremap = true, silent = true })
+-- 		end,
+-- 	},
+-- }
+
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", ";f", builtin.find_files, {})
-			vim.keymap.set("n", ";r", builtin.live_grep, {})
-			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { noremap = true, silent = true })
-		end,
+	"dmtrKovalenko/fff.nvim",
+	build = function()
+		require("fff.download").download_or_build_binary()
+	end,
+	opts = {
+		debug = {
+			enabled = false,
+			show_scores = false,
+		},
+	},
+	lazy = false,
+	keys = {
+		{
+			";f",
+			function()
+				require("fff").find_files()
+			end,
+			desc = "FFFind files",
+		},
+		{
+			";r",
+			function()
+				require("fff").live_grep()
+			end,
+			desc = "LiFFFe grep",
+		},
+		{
+			";w",
+			function()
+				require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+			end,
+			desc = "Search current word",
+		},
 	},
 }
